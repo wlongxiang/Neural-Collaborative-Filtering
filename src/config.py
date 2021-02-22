4bfc1e3d1dbafad38820f85f6fc3fa469b8e4c57
Benjamin Wang <wlongxiang1119@gmail.com>
2021-02-22 16:55:33 +0100

    temp commit

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 DATA_URL = "http://files.grouplens.org/datasets/movielens/ml-100k/u.data"
 
-MAIN_PATH = '/home/yyeon/KeepGo/Neural_Collaborative_Filtering/'
+MAIN_PATH = '/Users/longxiang/devel/github/UvAAI/thesis_ai/Neural-Collaborative-Filtering/'
 
 DATA_PATH = MAIN_PATH + 'data/ml-1m/ratings.dat'
 MODEL_PATH = MAIN_PATH + 'models/'
